0a1f2052046c555751a7c23df7c238cc9414a593
Atik Fahad <fahadatik@gmail.com>
2020-04-18 12:47:19 +0600

    add routes with lazy loading and use named router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,36 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import EventList from '../views/EventList.vue';
 
 Vue.use(VueRouter);
 
-// const routes = [
-//   {
-//     path: "/",
-//     name: "Home",
-//     component: Home
-//   },
-//   {
-//     path: "/about",
-//     name: "About",
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () =>
-//       import(/* webpackChunkName: "about" */ "../views/About.vue")
-//   }
-// ];
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  {
+    path: '/event',
+    name: 'event-show',
+    component: () =>
+      import(/* webpackChunkName: "event-show" */ '../views/EventShow.vue')
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "event-create" */ '../views/EventCreate.vue')
+  }
+];
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL
-  // routes
+  base: process.env.BASE_URL,
+  routes
 });
 
 export default router;
