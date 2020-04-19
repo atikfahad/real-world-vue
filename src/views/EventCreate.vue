b38299363a66b2f03294ad7396182c982088ebb0
Atik Fahad <fahadatik@gmail.com>
2020-04-19 17:18:55 +0600

    vuex(state, getters)

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['categories', 'user'])
+  }
+  // computed: mapState({
+  //   user: 'user',
+  //   categories: 'categories'
+  // })
+  // computed: mapState({
+  //     userName: state => state.user.name,
+  //     categories: state => state.categories
+  //   })
+  // computed: {
+  //   userName() {
+  //     return this.$store.state.user.name;
+  //   },
+  //   userID() {
+  //     return this.$store.state.user.id;
+  //   }
+  // }
+};
+</script>
+
+<style></style>
